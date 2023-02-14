383a20ed47b5e18d6aa4b8be12b497e62d8379e9
Gabriela <118011195+PaulaR-05@users.noreply.github.com>
2023-02-14 16:43:48 -0300

    update

--- js/script.js
+++ js/script.js
@@ -4,15 +4,30 @@ let lastName = document.getElementById("last-name");
 console.log(lastName);
 let password = document.getElementById("user-pass");
 console.log(password);
+let email = document.getElementById("user-email");
 
 function checkinfo() {
-  if (validName.value == "" || lastName.value == "" || password == "") {
+  if (validName.value == "") {
     document.getElementById("firstName-error").style.visibility = "visible";
+    document.querySelector(".disable").style.display = "block";
+  }
+
+  if (lastName.value == "") {
     document.getElementById("lastName-error").style.visibility = "visible";
-    document.getElementById("userPassword-error").style.visibility = "visible";
+    document.querySelector(".disable").style.display = "block";
   }
 }
 
+function checkEmail() {
+  let emailInfo = email.includes("@");
+  if (emailInfo == false || email.value == "") {
+    document.getElementById("userEmail-error").style.visibility = "visible";
+  }
+  return false;
+}
+console.log(checkEmail);
+
 function validate() {
   checkinfo();
+  checkEmail();
 }
