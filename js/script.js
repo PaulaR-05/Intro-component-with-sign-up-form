166714b0110513a323de759073bcb8cb3c62f1ac
Gabriela <118011195+PaulaR-05@users.noreply.github.com>
2023-02-15 16:19:04 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,44 +6,43 @@ let password = document.getElementById("user-pass");
 console.log(password);
 let email = document.getElementById("user-email");
 let validationMsg = document.querySelector(".validation-msg");
+let checkE = email.toString().includes("@");
 
-function checkinfo() {
+function validate() {
   if (
     validName.value != "" &&
     lastName.value != "" &&
-    email.value != "" &&
-    password.value != ""
+    password.value != "" &&
+    email.value != ""
   ) {
-    validationMsg.style.display = "block";
-  } else {
-    if (validName.value == "") {
-      document.getElementById("firstName-error").style.visibility = "visible";
-      document
-        .querySelector(".name-label")
-        .querySelector(".disable").style.display = "block";
-    }
-    if (lastName.value == "") {
-      document.getElementById("lastName-error").style.visibility = "visible";
-      document
-        .querySelector(".lastname-label")
-        .querySelector(".disable").style.display = "block";
-    }
-    if (email.value == "") {
-      document.getElementById("userEmail-error").style.visibility = "visible";
-      document
-        .querySelector(".email-label")
-        .querySelector(".disable").style.display = "block";
-    }
-    if (password.value == "") {
-      document.getElementById("userPassword-error").style.visibility =
-        "visible";
-      document
-        .querySelector(".pass-label")
-        .querySelector(".disable").style.display = "block";
-    }
+    validationMsg.innerHTML = "Success! Please check your email";
   }
+  checkinfo();
 }
 
-function validate() {
-  checkinfo();
+function checkinfo() {
+  if (validName.value == "") {
+    document.getElementById("firstName-error").style.visibility = "visible";
+    document
+      .querySelector(".name-label")
+      .querySelector(".disable").style.display = "block";
+  }
+  if (lastName.value == "") {
+    document.getElementById("lastName-error").style.visibility = "visible";
+    document
+      .querySelector(".lastname-label")
+      .querySelector(".disable").style.display = "block";
+  }
+  if (email.value == "") {
+    document.getElementById("userEmail-error").style.visibility = "visible";
+    document
+      .querySelector(".email-label")
+      .querySelector(".disable").style.display = "block";
+  }
+  if (password.value == "") {
+    document.getElementById("userPassword-error").style.visibility = "visible";
+    document
+      .querySelector(".pass-label")
+      .querySelector(".disable").style.display = "block";
+  }
 }
