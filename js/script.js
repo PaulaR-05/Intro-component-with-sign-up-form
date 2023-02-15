7dcdda0285f32d7359c1a27975f9e2c80960e96b
Gabriela <118011195+PaulaR-05@users.noreply.github.com>
2023-02-15 14:13:06 -0300

    update
    changes on js file

--- js/script.js
+++ js/script.js
@@ -5,45 +5,45 @@ console.log(lastName);
 let password = document.getElementById("user-pass");
 console.log(password);
 let email = document.getElementById("user-email");
+let validationMsg = document.querySelector(".validation-msg");
 
 function checkinfo() {
-  if (validName.value == "") {
-    document.getElementById("firstName-error").style.visibility = "visible";
-    document
-      .querySelector(".name-label")
-      .querySelector(".disable").style.display = "block";
-  }
-}
-
-function checkLastName() {
-  if (lastName.value == "") {
-    document.getElementById("lastName-error").style.visibility = "visible";
-    document
-      .querySelector(".lastname-label")
-      .querySelector(".disable").style.display = "block";
-  }
-}
-
-function checkEmail() {
-  if (email.value == "") {
-    document.getElementById("userEmail-error").style.visibility = "visible";
-    document
-      .querySelector(".email-label")
-      .querySelector(".disable").style.display = "block";
-  }
-}
-function checkPass() {
-  if (password.value == "") {
-    document.getElementById("userPassword-error").style.visibility = "visible";
-    document
-      .querySelector(".pass-label")
-      .querySelector(".disable").style.display = "block";
+  if (
+    validName.value != "" &&
+    lastName.value != "" &&
+    email.value != "" &&
+    password.value != ""
+  ) {
+    validationMsg.style.display = "block";
+  } else {
+    if (validName.value == "") {
+      document.getElementById("firstName-error").style.visibility = "visible";
+      document
+        .querySelector(".name-label")
+        .querySelector(".disable").style.display = "block";
+    }
+    if (lastName.value == "") {
+      document.getElementById("lastName-error").style.visibility = "visible";
+      document
+        .querySelector(".lastname-label")
+        .querySelector(".disable").style.display = "block";
+    }
+    if (email.value == "") {
+      document.getElementById("userEmail-error").style.visibility = "visible";
+      document
+        .querySelector(".email-label")
+        .querySelector(".disable").style.display = "block";
+    }
+    if (password.value == "") {
+      document.getElementById("userPassword-error").style.visibility =
+        "visible";
+      document
+        .querySelector(".pass-label")
+        .querySelector(".disable").style.display = "block";
+    }
   }
 }
 
 function validate() {
   checkinfo();
-  checkLastName();
-  checkPass();
-  checkEmail();
 }
